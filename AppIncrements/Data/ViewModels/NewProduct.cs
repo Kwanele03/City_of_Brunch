251f1546fe87cd657ee94f26c14d021ba8bdef28
Kwanele <112133974+Kwanele03@users.noreply.github.com>
2025-02-11 04:59:31 +0200

    Update NewProduct.cs

--- AppIncrements/Data/ViewModels/NewProduct.cs
+++ AppIncrements/Data/ViewModels/NewProduct.cs
@@ -6,19 +6,24 @@ namespace AppIncrements.Data.ViewModels
     {
         [Key]
         public int ProductId { get; set; }
+        
         [Display(Name = "Item")]
         [Required(ErrorMessage = "The Item Name is Required")]
         [StringLength(20, MinimumLength = 3, ErrorMessage = "The Item name must be between 3 to 20 chars")]
         public string Item { get; set; }
+        
         [Display(Name = "Photo")]
         [Required(ErrorMessage = "The Item Photo is Required")]
         public string Logo { get; set; }
+        
         [Display(Name = "Category")]
         [Required(ErrorMessage = "The Item Category is Required")]
         public string Category { get; set; }
+        
         [Display(Name = "Specification")]
         [Required(ErrorMessage = "The Item Specification is Required")]
         public string Specification { get; set; }
+        
         [Required(ErrorMessage = "The Item Price is Required")]
         public decimal Price { get; set; }
     }
